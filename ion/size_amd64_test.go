ba2ec2684f4d344cff50d6b0c9cd7a5e62c9f8f0
Wojciech Muła <wojciech@sneller.io>
2022-07-18 13:11:53 +0200

    ion: remove fastuv
    This function is unused and appeared to be not faster in practise.

--- ion/size_amd64_test.go
+++ ion/size_amd64_test.go
@@ -40,37 +40,3 @@ func TestIssue419(t *testing.T) {
 		}
 	}
 }
-
-func TestFastuv(t *testing.T) {
-	in := []uint{
-		0, 1, 7, 8, 15, 16, 17, 19, 100, 127, 128,
-		0x58, 0x43, 0x43 << 7,
-		(1 << 14) - 1, (1 << 14), (1 << 14) + 1,
-		(1 << 21) - 1, (1 << 21), (1 << 21) + 1,
-		12345,
-	}
-
-	var b Buffer
-	dst := make([]byte, 0, 16)
-	b.Set(dst)
-	for i := range in {
-		b.Reset()
-		u := in[i]
-		b.putuv(u)
-		bytes := b.Bytes()
-		p, _, ok := readuv(bytes)
-		if !ok {
-			t.Fatal("!ok")
-		}
-		if p != u {
-			t.Fatalf("portable %d from %d", p, u)
-		}
-		out, size := fastuv(bytes)
-		if out != u {
-			t.Errorf("0x%x: decoded as 0x%x (was 0x%x)", u, out, bytes)
-		}
-		if size != len(bytes) {
-			t.Errorf("%x: decoded as size %d (was %d)", u, size, len(bytes))
-		}
-	}
-}
