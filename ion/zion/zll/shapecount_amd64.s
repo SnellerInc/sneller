3378643afa42a706c937a950411c8fd9fbd71c21
Wojciech Muła <wojciech@sneller.io>
2023-01-31 14:46:01 +0000

    zll: faster shapecount
    Use an observation that in 60% of cases fc=15, and fc=16 is quite rare.
    
    benchmark                                old ns/op     new ns/op     delta
    BenchmarkShapecount/testdata/a.bin-8     602           221           -63.22%
    BenchmarkShapecount/testdata/b.bin-8     2309          867           -62.47%

--- ion/zion/zll/shapecount_amd64.s
+++ ion/zion/zll/shapecount_amd64.s
@@ -16,19 +16,26 @@
 #include "funcdata.h"
 #include "go_asm.h"
 
+/*
+    The procedure has three paths:
+    1. fast_path for fc = 15: we know that skip=9, count += 1, no check for data corruption
+    2. slow path for fc >= 15: we know that skip=9, count is unchanged,
+       and only then we check for fc > 16
+    3. common path for fc < 15: skip=(fc+3)/2, count += 1, no check for data corruption
+*/
 TEXT ·shapecount(SB), NOSPLIT, $0
     MOVQ shape_base+0(FP), SI
     MOVQ shape_len+8(FP), DX
     ADDQ SI, DX       // end-of-source
     XORL CX, CX       // count
-    XORL BX, BX       // zero
     JMP  loop_tail
 loop:
     MOVBLZX 0(SI), AX
     ANDL    $0x1f, AX // fc = shape[0] & 0x1f
-    CMPL    AX, $16   // assert(fc <= 16)
-    JA      corrupt
-    ADCXQ   BX, CX    // count++ if fc < 16
+    CMPL    AX, $15
+    JZ      fast_path
+    JA      slow_path
+    INCQ    CX
     ADDQ    $3, AX
     SHRQ    $1, AX    // skip = (fc + 3)/2
     ADDQ    AX, SI    // shape = shape[skip:]
@@ -38,6 +45,20 @@ loop_tail:
     JA      corrupt
     MOVQ    CX, ret+24(FP)
     RET
+fast_path: // fc = 15
+    INCQ    CX
+slow_path_tail:
+    ADDQ    $9, SI
+    CMPQ    SI, DX
+    JB      loop
+    JA      corrupt
+    MOVQ    CX, ret+24(FP)
+    RET
+slow_path: // fc >= 16 is rare
+    CMPL    AX, $16   // assert(fc <= 16)
+    JA      corrupt
+    JMP     slow_path_tail
+
 corrupt:
     MOVQ    $-1, ret+24(FP)
     RET
