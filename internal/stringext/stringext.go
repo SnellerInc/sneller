96f6791945cc7efb39466322ff42e6602696dd1a
Henk-Jan Lebbink <83118252+henkjan-sneller@users.noreply.github.com>
2023-08-21 17:22:55 +0200

    vm: removed double code, and removed min/max functions in stringext (#3167)
    Removed redundant min/max function (that are as of v1.21.0 part of Go). Additionally, cleanup of stringext.

--- internal/stringext/stringext.go
+++ internal/stringext/stringext.go
@@ -30,149 +30,97 @@ type Needle string
 // Data string type to distinguish from the Needle string type
 type Data = string
 
+// runeToUtf8Array encodes the provided rune to UTF8 in a byte array of length 4
 func runeToUtf8Array(r rune) []byte {
 	buf := make([]byte, 4)
 	utf8.EncodeRune(buf, r)
 	return buf
 }
 
-// alternativeRune gives all case-insensitive alternatives for the provided rune
-func alternativeRune(r0 rune) []rune {
-	r1 := unicode.SimpleFold(r0)
-	if r1 == r0 {
-		return []rune{r0}
-	}
-	r2 := unicode.SimpleFold(r1)
-	if (r2 == r0) || (r2 == r1) {
-		return []rune{r0, r1}
-	}
-	r3 := unicode.SimpleFold(r2)
-	if (r3 == r0) || (r3 == r1) || (r3 == r2) {
-		return []rune{r0, r1, r2}
-	}
-	return []rune{r0, r1, r2, r3}
-}
-
-func alternativeString(str string) (upper []rune, alt [][]rune) {
-
-	min2 := func(r0, r1 rune) rune {
-		if r0 < r1 {
-			return r0
-		}
-		return r1
-	}
-
-	min3 := func(r0, r1, r2 rune) rune {
-		return min2(r0, min2(r1, r2))
-	}
-
-	min4 := func(r0, r1, r2, r3 rune) rune {
-		return min2(r0, min3(r1, r2, r3))
-	}
-
-	runes := []rune(str)
-	nRunes := len(runes)
-	alt1 := make([]rune, nRunes)
-	alt2 := make([]rune, nRunes)
-	alt3 := make([]rune, nRunes)
-	alt4 := make([]rune, nRunes)
-	upper = make([]rune, nRunes)
-
-	inUse1, inUse2, inUse3, inUse4 := false, false, false, false
-
-	for i, r := range runes {
-		alt := alternativeRune(r)
-		switch len(alt) {
-		case 1:
-			alt1[i] = alt[0]
-			alt2[i] = alt[0]
-			alt3[i] = alt[0]
-			alt4[i] = alt[0]
-			upper[i] = alt[0]
-			inUse1 = true
-		case 2:
-			alt1[i] = alt[0]
-			alt2[i] = alt[1]
-			alt3[i] = alt[1]
-			alt4[i] = alt[1]
-			upper[i] = min2(alt[0], alt[1])
-			inUse2 = true
-		case 3:
-			alt1[i] = alt[0]
-			alt2[i] = alt[1]
-			alt3[i] = alt[2]
-			alt4[i] = alt[2]
-			upper[i] = min3(alt[0], alt[1], alt[2])
-			inUse3 = true
-		case 4:
-			alt1[i] = alt[0]
-			alt2[i] = alt[1]
-			alt3[i] = alt[2]
-			alt4[i] = alt[3]
-			upper[i] = min4(alt[0], alt[1], alt[2], alt[3])
-			inUse4 = true
-		}
-	}
-
-	if inUse4 {
-		return upper, [][]rune{alt1, alt2, alt3, alt4}
-	}
-	if inUse3 {
-		return upper, [][]rune{alt1, alt2, alt3}
-	}
-	if inUse2 {
-		return upper, [][]rune{alt1, alt2}
-	}
-	if inUse1 {
-		return upper, [][]rune{alt1}
-	}
-	return upper, [][]rune{} // unreachable
-}
-
+// to4ByteArray encode provided int to uint32 with little endian encoding in a byte array (of len 4)
 func to4ByteArray(value int) []byte {
 	buf := make([]byte, 4)
 	binary.LittleEndian.PutUint32(buf, uint32(value))
 	return buf
 }
 
-// genNeedleUTF8Ci generates an extended string representation needed for UTF8 ci comparisons
-func genNeedleUTF8Ci(needle string, reversed bool) string {
+func stringAlternatives(str string, reversed, addASCII, addBounds bool) (alt []byte) {
 
-	stringAlternatives := func(str string, reversed bool) (alt []byte) {
-		nRunes := utf8.RuneCountInString(str)
-		alt = make([]byte, 0) // alt = alternative list of runes encoded as utf8 code-points stored in 32bits int
-		upperRunes, altArr := alternativeString(str)
-
-		upper := []byte(string(upperRunes))
-		upper = append(upper, 0, 0, 0)
-
-		var alt1, alt2, alt3, alt4 []rune
-		switch len(altArr) {
-		case 1:
-			alt1 = altArr[0]
-			alt2 = altArr[0]
-			alt3 = altArr[0]
-			alt4 = altArr[0]
-		case 2:
-			alt1 = altArr[0]
-			alt2 = altArr[1]
-			alt3 = altArr[1]
-			alt4 = altArr[1]
-		case 3:
-			alt1 = altArr[0]
-			alt2 = altArr[1]
-			alt3 = altArr[2]
-			alt4 = altArr[2]
-		case 4:
-			alt1 = altArr[0]
-			alt2 = altArr[1]
-			alt3 = altArr[2]
-			alt4 = altArr[3]
+	// maximal length (in bytes) of data that can match with the needle
+	byteLengthBounds := func(pos int, runeAlternatives [][4]rune) (lowerBound, upperBound int) {
+		for i := pos; i < len(runeAlternatives); i++ {
+			nBytes0 := utf8.RuneLen(runeAlternatives[i][0])
+			nBytes1 := utf8.RuneLen(runeAlternatives[i][1])
+			nBytes2 := utf8.RuneLen(runeAlternatives[i][2])
+			nBytes3 := utf8.RuneLen(runeAlternatives[i][3])
+			upperBound += max(nBytes0, nBytes1, nBytes2, nBytes3)
+			lowerBound += min(nBytes0, nBytes1, nBytes2, nBytes3)
 		}
+		return lowerBound, upperBound
+	}
 
+	// alternativeRune gives all case-insensitive alternatives for the provided rune
+	alternativeRune := func(r0 rune) [4]rune {
+		r1 := unicode.SimpleFold(r0)
+		if r1 == r0 {
+			return [4]rune{r0, r0, r0, r0}
+		}
+		r2 := unicode.SimpleFold(r1)
+		if (r2 == r0) || (r2 == r1) {
+			x := []rune{r0, r1}
+			slices.Sort(x)
+			return [4]rune{x[0], x[1], x[1], x[1]}
+		}
+		r3 := unicode.SimpleFold(r2)
+		if (r3 == r0) || (r3 == r1) || (r3 == r2) {
+			x := []rune{r0, r1, r2}
+			slices.Sort(x)
+			return [4]rune{x[0], x[1], x[2], x[2]}
+		}
+		x := []rune{r0, r1, r2, r3}
+		slices.Sort(x)
+		return ([4]rune)(x)
+	}
+
+	// reverseSlice reversed the provided slide
+	reverseSlice := func(slice []int) {
+		for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+			slice[i], slice[j] = slice[j], slice[i]
+		}
+	}
+
+	nRunes := utf8.RuneCountInString(str)
+	alt = make([]byte, 0) // alt = alternative list of runes encoded as utf8 code-points stored in 32bits int
+
+	runes := []rune(str)
+	runeAlternatives := make([][4]rune, nRunes)
+	upperRunes := make([]rune, nRunes)
+
+	for i, r := range runes {
+		runeAlternatives[i] = alternativeRune(r)
+		upperRunes[i] = min(runeAlternatives[i][0], runeAlternatives[i][1], runeAlternatives[i][2], runeAlternatives[i][3])
+	}
+
+	upper := []byte(string(upperRunes))
+	upper = append(upper, 0, 0, 0)
+
+	lowerBounds := make([]int, nRunes)
+	if addBounds {
 		for i := 0; i < nRunes; i++ {
-			if reversed {
+			lowerBound, _ := byteLengthBounds(i, runeAlternatives)
+			lowerBounds[i] = lowerBound
+		}
+		if reversed {
+			reverseSlice(lowerBounds)
+		}
+	}
 
+	for i := 0; i < nRunes; i++ {
+		if reversed {
+			if addBounds {
+				alt = append(to4ByteArray(lowerBounds[i]), alt...)
+			}
+			if addASCII {
 				x3 := byte(0)
 				if i-3 >= 0 {
 					x3 = upper[i-3]
@@ -190,25 +138,25 @@ func genNeedleUTF8Ci(needle string, reversed bool) string {
 					x0 = upper[i-0]
 				}
 				alt = append([]byte{x3, x2, x1, x0}, alt...)
-				alt = append(runeToUtf8Array(alt1[i]), alt...)
-				alt = append(runeToUtf8Array(alt2[i]), alt...)
-				alt = append(runeToUtf8Array(alt3[i]), alt...)
-				alt = append(runeToUtf8Array(alt4[i]), alt...)
-			} else {
-				alt = append(alt, runeToUtf8Array(alt1[i])...)
-				alt = append(alt, runeToUtf8Array(alt2[i])...)
-				alt = append(alt, runeToUtf8Array(alt3[i])...)
-				alt = append(alt, runeToUtf8Array(alt4[i])...)
+			}
+			alt = append(runeToUtf8Array(runeAlternatives[i][0]), alt...)
+			alt = append(runeToUtf8Array(runeAlternatives[i][1]), alt...)
+			alt = append(runeToUtf8Array(runeAlternatives[i][2]), alt...)
+			alt = append(runeToUtf8Array(runeAlternatives[i][3]), alt...)
+		} else {
+			alt = append(alt, runeToUtf8Array(runeAlternatives[i][0])...)
+			alt = append(alt, runeToUtf8Array(runeAlternatives[i][1])...)
+			alt = append(alt, runeToUtf8Array(runeAlternatives[i][2])...)
+			alt = append(alt, runeToUtf8Array(runeAlternatives[i][3])...)
+			if addASCII {
 				alt = append(alt, upper[i+0], upper[i+1], upper[i+2], upper[i+3])
 			}
+			if addBounds {
+				alt = append(alt, to4ByteArray(lowerBounds[i])...)
+			}
 		}
-		return
 	}
-
-	result := make([]byte, 0)
-	result = append(result, to4ByteArray(utf8.RuneCountInString(needle))...)
-	result = append(result, stringAlternatives(needle, reversed)...)
-	return string(result)
+	return
 }
 
 // fuzzyApprox3Spec returns the data-structure for the fuzzy approximation (with 3 characters lookahead)
@@ -859,41 +807,11 @@ func (t StrCmpType) String() string {
 	return "??"
 }
 
-func stringAlternatives(str string) (alt []byte) {
-	nRunes := utf8.RuneCountInString(str)
-	alt = make([]byte, 0) // alt = alternative list of runes encoded as utf8 code-points stored in 32bits int
-	_, altArr := alternativeString(str)
-
-	var alt1, alt2, alt3, alt4 []rune
-	switch len(altArr) {
-	case 1:
-		alt1 = altArr[0]
-		alt2 = altArr[0]
-		alt3 = altArr[0]
-		alt4 = altArr[0]
-	case 2:
-		alt1 = altArr[0]
-		alt2 = altArr[1]
-		alt3 = altArr[1]
-		alt4 = altArr[1]
-	case 3:
-		alt1 = altArr[0]
-		alt2 = altArr[1]
-		alt3 = altArr[2]
-		alt4 = altArr[2]
-	case 4:
-		alt1 = altArr[0]
-		alt2 = altArr[1]
-		alt3 = altArr[2]
-		alt4 = altArr[3]
-	}
-	for i := 0; i < nRunes; i++ {
-		alt = append(alt, runeToUtf8Array(alt1[i])...)
-		alt = append(alt, runeToUtf8Array(alt2[i])...)
-		alt = append(alt, runeToUtf8Array(alt3[i])...)
-		alt = append(alt, runeToUtf8Array(alt4[i])...)
-	}
-	return
+// genNeedleUTF8Ci generates an extended string representation needed for UTF8 ci comparisons
+func genNeedleUTF8Ci(needle Needle, reversed, addASCII, addBounds bool) string {
+	result := to4ByteArray(utf8.RuneCountInString(string(needle)))
+	result = append(result, stringAlternatives(string(needle), reversed, addASCII, addBounds)...)
+	return string(result)
 }
 
 // EncodeEqualStringCS encodes the provided string for usage with bcStrCmpEqCs
@@ -909,7 +827,7 @@ func EncodeEqualStringCI(needle Needle) string {
 
 // EncodeEqualStringUTF8CI encodes the provided string for usage with bcStrCmpEqUTF8Ci
 func EncodeEqualStringUTF8CI(needle Needle) string {
-	return genNeedleUTF8Ci(string(needle), false)
+	return genNeedleUTF8Ci(needle, false, true, false)
 }
 
 // EncodeContainsPrefixCS encodes the provided string for usage with bcContainsPrefixCs
@@ -924,7 +842,7 @@ func EncodeContainsPrefixCI(needle Needle) string {
 
 // EncodeContainsPrefixUTF8CI encodes the provided string for usage with bcContainsPrefixUTF8Ci
 func EncodeContainsPrefixUTF8CI(needle Needle) string {
-	return genNeedleUTF8Ci(string(needle), false)
+	return genNeedleUTF8Ci(needle, false, true, false)
 }
 
 // EncodeContainsSuffixCS encodes the provided string for usage with bcContainsSuffixCs
@@ -939,7 +857,7 @@ func EncodeContainsSuffixCI(needle Needle) string {
 
 // EncodeContainsSuffixUTF8CI encodes the provided string for usage with bcContainsSuffixUTF8Ci
 func EncodeContainsSuffixUTF8CI(needle Needle) string {
-	return genNeedleUTF8Ci(string(needle), true)
+	return genNeedleUTF8Ci(needle, true, true, false)
 }
 
 // EncodeContainsSubstrCS encodes the provided string for usage with bcContainsSubstrCs
@@ -954,9 +872,7 @@ func EncodeContainsSubstrCI(needle Needle) string {
 
 // EncodeContainsSubstrUTF8CI encodes the provided string for usage with bcContainsSubstrUTF8Ci
 func EncodeContainsSubstrUTF8CI(needle Needle) string {
-	result := to4ByteArray(utf8.RuneCountInString(string(needle)))
-	result = append(result, stringAlternatives(string(needle))...)
-	return string(result)
+	return genNeedleUTF8Ci(needle, false, false, false)
 }
 
 // EncodeContainsPatternCS encodes the provided string for usage with bcContainsPatternCs
@@ -985,7 +901,7 @@ func EncodeContainsPatternCI(pattern *Pattern) string {
 // EncodeContainsPatternUTF8CI encodes the provided string for usage with bcContainsPatternUTF8Ci
 func EncodeContainsPatternUTF8CI(pattern *Pattern) string {
 	result := to4ByteArray(utf8.RuneCountInString(string(pattern.Needle)))
-	result = append(result, stringAlternatives(string(pattern.Needle))...)
+	result = append(result, stringAlternatives(string(pattern.Needle), false, false, false)...)
 	needleRune := []rune(pattern.Needle)
 
 	for i := 0; i < len(needleRune); i++ {
@@ -1105,7 +1021,7 @@ func NewPattern(str string, wc, escape rune) Pattern {
 }
 
 // SplitWC splits the needle on wc and concatenates consecutive wildcards,
-// eg. "a__b", (with WC = '_') becomes ["a", "__", "b"], [[false], [true, true], [false]]
+// e.g. "a__b", (with WC = '_') becomes ["a", "__", "b"], [[false], [true, true], [false]]
 func (p Pattern) SplitWC() ([]Needle, [][]bool) {
 	needles := make([]Needle, 0)
 	wildcards := make([][]bool, 0)
